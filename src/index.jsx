a7044e76a5dbf964a74c4742df10aafea9f975ba
leninner <mazabandalenin180@gmail.com>
2022-02-13 21:49:28 -0500

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './routes/App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { reducer } from './reducers';
 import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
