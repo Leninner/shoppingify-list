2c9074b390cb8b093e742de3e40abd366080112e
leninner <mazabandalenin180@gmail.com>
2022-02-16 15:39:58 -0500

    Update reducers

--- src/reducers/categoriesReducer.js
+++ src/reducers/categoriesReducer.js
@@ -28,174 +28,130 @@ const initalState = {
             'https://images.unsplash.com/photo-1612871689353-cccf581d667b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
           isItemInfo: false,
         },
-        {
-          id: 3,
-          itemName: 'Chuleta de cerdo',
-          note: 'La carne es algo riquísimo',
-          imageURL:
-            'https://images.unsplash.com/photo-1612871689353-cccf581d667b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-          isItemInfo: false,
-        },
-        {
-          id: 4,
-          itemName: 'Huesos de pollo',
-          note: 'La carne es algo riquísimo',
-          imageURL:
-            'https://images.unsplash.com/photo-1612871689353-cccf581d667b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-          isItemInfo: false,
-        },
       ],
     },
     {
       id: 2,
       name: 'Meet and Fish',
-      items: [
-        {
-          id: 1,
-          itemName: 'Uvas',
-          note: 'La carne es algo riquísimo',
-          imageURL:
-            'https://images.unsplash.com/photo-1612871689353-cccf581d667b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-          isItemInfo: false,
-        },
-        {
-          id: 2,
-          itemName: 'Manzanas',
-          note: 'La carne es algo riquísimo',
-          imageURL:
-            'https://images.unsplash.com/photo-1612871689353-cccf581d667b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-          isItemInfo: false,
-        },
-        {
-          id: 3,
-          itemName: 'Peras',
-          note: 'La carne es algo riquísimo',
-          imageURL:
-            'https://images.unsplash.com/photo-1612871689353-cccf581d667b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-          isItemInfo: false,
-        },
-        {
-          id: 4,
-          itemName: 'Frutillas',
-          note: 'La carne es algo riquísimo',
-          imageURL:
-            'https://images.unsplash.com/photo-1612871689353-cccf581d667b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-          isItemInfo: false,
-        },
-      ],
+      items: [],
     },
   ],
   isAdding: false,
 };
 
 export const categoriesReducer = (state = initalState, action) => {
-  switch (action.type) {
-    case TOGGLE_ADD_ITEM:
-      return {
-        ...state,
-        isAdding: !state.isAdding,
-      };
-    case ADD_ITEM_TO_CATEGORY:
-      const categoriesUpdated = [...state.categories];
+  if (action.type === TOGGLE_ADD_ITEM) {
+    return {
+      ...state,
+      isAdding: !state.isAdding,
+    };
+  }
 
-      const { category } = action.payload;
+  if (action.type === ADD_ITEM_TO_CATEGORY) {
+    const categoriesUpdated = [...state.categories];
 
-      if (categoriesUpdated.some(({ name }) => name === category)) {
-        const categoryFind = categoriesUpdated.find((value) => value.name === category).items;
+    const { category } = action.payload;
 
-        const itemToAdd = {
-          id: categoryFind.length + 1,
-          note: action.payload.note,
-          imageURL: action.payload.imageURL,
-          itemName: action.payload.itemName,
-          isItemInfo: false,
-        };
+    const { note, imageURL, itemName } = action.payload;
 
-        categoryFind.push({
-          ...itemToAdd,
-        });
-      } else {
-        categoriesUpdated.push({
-          id: categoriesUpdated.length + 1,
-          name: category,
-          items: [
-            {
-              id: 1,
-              note: action.payload.note,
-              imageURL: action.payload.imageURL,
-              itemName: action.payload.itemName,
-              isItemInfo: false,
-            },
-          ],
-        });
-      }
+    if (categoriesUpdated.some(({ name }) => name === category)) {
+      const categoryFind = categoriesUpdated.find((value) => value.name === category).items;
 
-      return {
-        ...state,
-        categories: [...categoriesUpdated],
+      const itemToAdd = {
+        id: categoryFind.length + 1,
+        note: note,
+        imageURL: imageURL,
+        itemName: itemName,
+        isItemInfo: false,
       };
 
-    case SEE_INFO_ITEM:
-      const categoriesUpdated2 = [...state.categories];
-      const { idItem: idSeeItem, idCategory: idSeeCategory } = action.payload;
-
-      // Buscar una mejor manera de volver el valor de isItemInfo a false
-      categoriesUpdated2.map((value) => {
-        value.items.map((item) => {
-          item.isItemInfo = false;
-          return item;
-        });
-        return value;
+      categoryFind.push({
+        ...itemToAdd,
       });
+    } else {
+      categoriesUpdated.push({
+        id: categoriesUpdated.length + 1,
+        name: category,
+        items: [
+          {
+            id: 1,
+            note: note,
+            imageURL: imageURL,
+            itemName: itemName,
+            isItemInfo: false,
+          },
+        ],
+      });
+    }
 
-      categoriesUpdated2[idSeeCategory - 1].items[idSeeItem - 1].isItemInfo = true;
+    return {
+      ...state,
+      categories: [...categoriesUpdated],
+    };
+  }
 
-      return {
-        ...state,
-        categories: [...categoriesUpdated2],
-      };
+  if (action.type === SEE_INFO_ITEM) {
+    const categoriesUpdated = [...state.categories];
+    const { idItem: idSeeItem, idCategory: idSeeCategory } = action.payload;
 
-    case BACK_TO_SHOPPING_LIST:
-      const categoriesUpdated3 = [...state.categories];
-      const { idItem, idCategory } = action.payload;
-      categoriesUpdated3[idCategory - 1].items[idItem - 1].isItemInfo = false;
-
-      return {
-        ...state,
-        categories: [...categoriesUpdated3],
-      };
-
-    case DELETE_ITEM_FROM_CATEGORIES:
-      const categoriesUpdated4 = [...state.categories];
-      const { payload, shoppingCart } = action;
-
-      const { idItem: idItemToDelete, categoryName, itemName, idCategory: idCategoryToDelete } = payload;
-
-      // Método para eliminar un item de una categoría y actualizar los items de shopping cart
-      shoppingCart[idCategoryToDelete - 1].items.map((item, index) => {
-        if (item.itemName === itemName) {
-          shoppingCart[idCategoryToDelete - 1].items.splice(index, 1);
-        }
-
+    // Buscar una mejor manera de volver el valor de isItemInfo a false
+    categoriesUpdated.map((value) => {
+      value.items.map((item) => {
+        item.isItemInfo = false;
         return item;
       });
+      return value;
+    });
 
-      categoriesUpdated4.map((value) => {
-        if (value.name === categoryName) {
-          value.items.splice(idItemToDelete - 1, 1);
-          value.items.map((item, index) => (item.id = index + 1));
-        }
+    categoriesUpdated[idSeeCategory - 1].items[idSeeItem - 1].isItemInfo = true;
 
-        return value;
-      });
-
-      return {
-        ...state,
-        categories: [...categoriesUpdated4],
-        shoppingCart: [...shoppingCart],
-      };
-
-    default:
-      return state;
+    return {
+      ...state,
+      categories: [...categoriesUpdated],
+    };
   }
+
+  if (action.type === BACK_TO_SHOPPING_LIST) {
+    const categoriesUpdated = [...state.categories];
+    const { idItem, idCategory } = action.payload;
+    categoriesUpdated[idCategory - 1].items[idItem - 1].isItemInfo = false;
+
+    return {
+      ...state,
+      categories: [...categoriesUpdated],
+    };
+  }
+
+  if (action.type === DELETE_ITEM_FROM_CATEGORIES) {
+    const categoriesUpdated = [...state.categories];
+    const { payload, shoppingCart } = action;
+
+    const { idItem: idItemToDelete, categoryName, itemName, idCategory: idCategoryToDelete } = payload;
+
+    // Método para eliminar un item de una categoría y actualizar los items de shopping cart
+    shoppingCart[idCategoryToDelete - 1].items.map((item, index) => {
+      if (item.itemName === itemName) {
+        shoppingCart[idCategoryToDelete - 1].items.splice(index, 1);
+      }
+
+      return item;
+    });
+
+    categoriesUpdated.map((value) => {
+      if (value.name === categoryName) {
+        value.items.splice(idItemToDelete - 1, 1);
+        value.items.map((item, index) => (item.id = index + 1));
+      }
+
+      return value;
+    });
+
+    return {
+      ...state,
+      categories: [...categoriesUpdated],
+      shoppingCart: [...shoppingCart],
+    };
+  }
+
+  return state;
 };
--- src/reducers/shoppingCartReducer.js
+++ src/reducers/shoppingCartReducer.js
@@ -16,66 +16,61 @@ const initialState = {
 };
 
 export const shoppingCartReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TO_CURRENT_LIST:
-      const { payload, categories } = action;
-      const { idItem, idCategory } = payload;
+  if (action.type === ADD_TO_CURRENT_LIST) {
+    const { payload, categories } = action;
+    const { idItem, idCategory } = payload;
 
-      const newShoppingCart = [...state.shoppingCart];
-      const itemToAdd = categories[idCategory - 1].items[idItem - 1];
-      itemToAdd.quantity = 1;
-      newShoppingCart[idCategory - 1].items.push(itemToAdd);
+    const newShoppingCart = [...state.shoppingCart];
+    const itemToAdd = categories[idCategory - 1].items[idItem - 1];
+    itemToAdd.quantity = 1;
+    newShoppingCart[idCategory - 1].items.push(itemToAdd);
 
-      const categoriesUpdated = [...categories];
-      categoriesUpdated[idCategory - 1].items[idItem - 1].isItemInfo = false;
+    const categoriesUpdated = [...categories];
+    categoriesUpdated[idCategory - 1].items[idItem - 1].isItemInfo = false;
 
-      return {
-        ...state,
-        shoppingCart: [...newShoppingCart],
-        categories: [...categoriesUpdated],
-      };
-
-    case EDIT_ITEM_QUANTITY:
-      console.log(action.payload);
-      const { idCategory: idCategoryToUpdate, idItem: idItemToUpdate, itemName, newQuantity } = action.payload;
-      const newShoppingCart2 = [...state.shoppingCart];
-
-      newShoppingCart2[idCategoryToUpdate - 1].items.map((item) => {
-        if (item.id === idItemToUpdate && item.itemName === itemName) {
-          item.quantity = newQuantity;
-        }
-        return item;
-      });
-
-      return {
-        ...state,
-        shoppingCart: [...newShoppingCart2],
-      };
-
-    case DELETE_ITEM_FROM_CURRENT_LIST:
-      console.log(action.payload);
-      const { idCategory: idCategoryToDelete, idItem: idItemToDelete, itemName: itemNameToDelete } = action.payload;
-
-      const newShoppingCart3 = [...state.shoppingCart];
-
-      newShoppingCart3[idCategoryToDelete - 1].items.map((item) => {
-        if (item.id === idItemToDelete && item.itemName === itemNameToDelete) {
-          newShoppingCart3[idCategoryToDelete - 1].items.splice(
-            newShoppingCart3[idCategoryToDelete - 1].items.indexOf(item),
-            1
-          );
-        }
-
-        return item;
-      });
-
-      return {
-        ...state,
-        shoppingCart: [...newShoppingCart3],
-      };
-    default:
-      return state;
+    return {
+      ...state,
+      shoppingCart: [...newShoppingCart],
+      categories: [...categoriesUpdated],
+    };
   }
+
+  if (action.type === EDIT_ITEM_QUANTITY) {
+    const { idCategory, idItem, itemName, newQuantity } = action.payload;
+    const newShoppingCart = [...state.shoppingCart];
+
+    newShoppingCart[idCategory - 1].items.map((item) => {
+      if (item.id === idItem && item.itemName === itemName) {
+        item.quantity = newQuantity;
+      }
+      return item;
+    });
+
+    return {
+      ...state,
+      shoppingCart: [...newShoppingCart],
+    };
+  }
+
+  if (action.type === DELETE_ITEM_FROM_CURRENT_LIST) {
+    const { idCategory, idItem, itemName } = action.payload;
+    const newShoppingCart = [...state.shoppingCart];
+
+    newShoppingCart[idCategory - 1].items.map((item) => {
+      if (item.id === idItem && item.itemName === itemName) {
+        newShoppingCart[idCategory - 1].items.splice(newShoppingCart[idCategory - 1].items.indexOf(item), 1);
+      }
+
+      return item;
+    });
+
+    return {
+      ...state,
+      shoppingCart: [...newShoppingCart],
+    };
+  }
+
+  return state;
 };
 
 //  {
