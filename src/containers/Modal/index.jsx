bc6ce2aaf4749d30a11eef376c103468e6981fce
leninner <mazabandalenin180@gmail.com>
2022-02-21 17:07:01 -0500

    Update

--- src/containers/Modal/index.jsx
+++ src/containers/Modal/index.jsx
@@ -1,4 +1,4 @@
 import { createPortal } from 'react-dom';
-import { ConfirmListPopup } from '../ConfirmListPopup';
+import { ConfirmListPopup } from '../../components/ConfirmListPopup';
 
 export const Modal = () => createPortal(<ConfirmListPopup />, document.getElementById('modal_root'));
