fbd0012c3bb6272ba44604e245eaaf6b99294a8f
leninner <mazabandalenin180@gmail.com>
2022-02-21 14:42:35 -0500

    Update

--- src/reducers/historialReducer.js
+++ src/reducers/historialReducer.js
@@ -8,11 +8,13 @@ export const historialReducer = (state = initialState, action) => {
   if (type === 'ADD_TO_HISTORY') {
     const { shoppingCart, shoppingListName } = action.payload;
 
+    const shoppingCartFiltered = shoppingCart.filter((value) => value.items.length > 0);
+
     const historyToAdd = {
       id: state.historial.length + 1,
       shoppingListName: shoppingListName,
       dateCompleted: new Date().toDateString(),
-      shoppingList: [...shoppingCart],
+      shoppingList: [...shoppingCartFiltered],
     };
 
     return {
