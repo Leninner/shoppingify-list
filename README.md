11492c48dddb0a00c6878349ac8de94446a18eaf
leninner <mazabandalenin180@gmail.com>
2022-02-28 17:45:56 -0500

    Added Redux Persist

--- README.md
+++ README.md
@@ -121,6 +121,7 @@ $ npm start
 - [FAQs Redux](https://redux.js.org/faq/reducers/#how-do-i-share-state-between-two-reducers-do-i-have-to-use-combinereducers)
 - [PX to REM Converter](https://nekocalc.com/es/px-a-rem-conversor)
 - [Warning: Can't perform a React state update on an unmounted component.](https://stackoverflow.com/questions/53949393/cant-perform-a-react-state-update-on-an-unmounted-component)
+- [Redux Persist](https://www.npmjs.com/package/redux-persist#basic-usage)
 
 ## Contact
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './routes/App';
 import { Provider } from 'react-redux';
-import { reducer } from './reducers';
+import { reducer as rootReducer } from './reducers';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+// const store = createStore(reducer, applyMiddleware(thunk));
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = createStore(persistedReducer, applyMiddleware(thunk));
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
